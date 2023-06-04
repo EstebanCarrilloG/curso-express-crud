c833253497adc7fd0fe664783b09ab1c4bf52171
Esteban Carrillo <estexav0996@gmail.com>
2023-06-04 15:36:16 -0500

    nada

---
+++ cursos.js
@@ -0,0 +1,45 @@
+let infoCursos = {
+  'programacion': [
+    {
+      id: 1,
+      titulo: 'Aprende Pyton',
+      lenguaje: 'python',
+      vistas: 12552,
+      nivel: 'basico'
+    },
+    {
+      id: 2,
+      titulo: 'Pyton Intermedio',
+      lenguaje: 'pyton',
+      vistas: 2356,
+      nivel: 'intermedio'
+    },
+    {
+      id: 3,
+      titulo: 'Curso Javascript',
+      lenguaje: 'javaScript',
+      vistas: 12552235,
+      nivel: 'basico'
+    }
+  ],
+  'matematicas': [
+    {
+      id: 1,
+      titulo: 'Calculo',
+      tema: 'calculo',
+      vistas: 12552235,
+      nivel: 'basico'
+    },
+    {
+      id: 2,
+      titulo: 'algebra',
+      tema: 'algebra',
+      vistas: 2235,
+      nivel: 'basico'
+
+    }
+
+  ]
+}
+
+module.exports.infoCursos = infoCursos;
