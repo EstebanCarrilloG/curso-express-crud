cc55ed74e3581ae122e8bbdf08b7392184d8a657
Esteban Carrillo <estexav0996@gmail.com>
2023-06-04 17:22:15 -0500

    nothing

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
 
-const { infoCursos } = require('./cursos.js');
+const routerMatematicas = require('./routers/matematicas.js');
+const routerProgramacion = require('./routers/programacion.js');
 
 //Routers
 
-const routerProgramacion = express.Router();
-
 app.use('/api/cursos/programacion', routerProgramacion)
-
-const routerMatematicas = express.Router();
 
 app.use('/api/cursos/matematicas', routerMatematicas);
 
@@ -23,60 +20,7 @@ app.get('/api/cursos',(req,res) =>{
     res.send( JSON.stringify(infoCursos));
 });
 
-routerProgramacion.get('/',(req,res) =>{
-    res.send(JSON.stringify(infoCursos.programacion));
-});
-
-routerMatematicas.get('/',(req,res) =>{
-    res.send(JSON.stringify(infoCursos.matematicas));
-});
-
-routerProgramacion.get('/:lenguaje',(req,res) =>{
-    const lenguaje = req.params.lenguaje;
-    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje );
-    if(resultados.length === 0){
-        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
-    }
-
-console.log(req.query.ordenar);
-
-if(req.query.ordenar === 'vistas'){
-    return res.send(JSON.stringify(resultados.sort((a,b) => b.vistas - a.vistas)));
-
-}
-
-    res.send(JSON.stringify(resultados));
-});
-
-routerMatematicas.get('/:tema',(req,res)=>{
-    const tema = req.params.tema;
-    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema);
-    if(resultados.length === 0){
-        return res.status(404).send(`No se encintraron cursos de ${tema}`)
-    }
-
-    res.send(JSON.stringify(resultados))
-})
-
-routerProgramacion.get('/:lenguaje/:nivel', (req,res) =>{
-    const lenguaje = req.params.lenguaje;
-    const nivel = req.params.nivel;
-
-    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel);
-
-    if(resultados.length === 0){
-        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}.`);
-    }
-
-    res.send(JSON.stringify(resultados))
-})
-
-
-
-
-
 const PUERTO = process.env.PORT || 3010;  
-
 
 app.listen(PUERTO, () => {
    console.log(`El servidor esta funcionando en el puerto ${PUERTO}`);
