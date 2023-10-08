3935ad16944a25dccc24c63faf7fd68be30501d5
Esteban Carrillo <estexav0996@gmail.com>
2023-10-08 15:46:11 -0500

    Code formated

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const routerMatematicas = require('./routers/matematicas.js');
-const routerProgramacion = require('./routers/programacion.js');
+const routerMatematicas = require("./routers/matematicas.js");
+const routerProgramacion = require("./routers/programacion.js");
 
 //Routers
 
-app.use('/api/cursos/programacion', routerProgramacion)
+app.use("/api/cursos/programacion", routerProgramacion);
 
-app.use('/api/cursos/matematicas', routerMatematicas);
+app.use("/api/cursos/matematicas", routerMatematicas);
 
 // Routing
 
-app.get('/', (req, res) => {
-    res.send('Mi primer servidor con express.');
+app.get("/", (req, res) => {
+  res.send("Mi primer servidor con express.");
 });
 
-app.get('/api/cursos', (req, res) => {
-    res.send(JSON.stringify(infoCursos));
+app.get("/api/cursos", (req, res) => {
+  res.send(JSON.stringify(infoCursos));
 });
 
 const PUERTO = process.env.PORT || 3010;
 
 app.listen(PUERTO, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PUERTO}`);
-}); 
+  console.log(`El servidor esta funcionando en el puerto ${PUERTO}`);
+});
--- datos/cursos.js
+++ datos/cursos.js
@@ -1,45 +1,43 @@
 let infoCursos = {
-  'programacion': [
+  programacion: [
     {
       id: 1,
-      titulo: 'Aprende Pyton',
-      lenguaje: 'python',
+      titulo: "Aprende Pyton",
+      lenguaje: "python",
       vistas: 12552,
-      nivel: 'basico'
+      nivel: "basico",
     },
     {
       id: 2,
-      titulo: 'Pyton Intermedio',
-      lenguaje: 'python',
+      titulo: "Pyton Intermedio",
+      lenguaje: "python",
       vistas: 2356,
-      nivel: 'intermedio'
+      nivel: "intermedio",
     },
     {
       id: 3,
-      titulo: 'Curso Javascript',
-      lenguaje: 'javaScript',
+      titulo: "Curso Javascript",
+      lenguaje: "javaScript",
       vistas: 12552235,
-      nivel: 'basico'
-    }
+      nivel: "basico",
+    },
   ],
-  'matematicas': [
+  matematicas: [
     {
       id: 1,
-      titulo: 'Calculo',
-      tema: 'calculo',
+      titulo: "Calculo",
+      tema: "calculo",
       vistas: 12552235,
-      nivel: 'basico'
+      nivel: "basico",
     },
     {
       id: 2,
-      titulo: 'algebra',
-      tema: 'algebra',
+      titulo: "algebra",
+      tema: "algebra",
       vistas: 2235,
-      nivel: 'basico'
-
-    }
-
-  ]
-}
+      nivel: "basico",
+    },
+  ],
+};
 
 module.exports.infoCursos = infoCursos;
